65e07b3ef3716613d95350b4d96c8f81869d3b85
Prissee <queenpris24@gmail.com>
2023-07-20 16:17:09 -0700

    more loops

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,19 +7,15 @@
  *
  *   Return: Always 0 ( Success)
  */
+
 void print_square(int size)
 {
-	if (size <= 0)
-	{
-	_putchar('\n');
-	}
-	else
-	{
-	int i;
+	int row, column;
 
-	for (i = 0; i < size; i++)
+	for (row = 1; row <= size; ++row)
 	{
+	for (column = 1; column <= size; ++column)
+	_putchar('#');
 	_putchar('\n');
-	}
 	}
 }
